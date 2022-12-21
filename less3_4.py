adae272880d8de2295d1d7eeddecf1b106882cf7
litriq <119163655+litriq@users.noreply.github.com>
2022-12-21 13:46:37 +0300

    Update less3_4.py

--- less3_4.py
+++ less3_4.py
@@ -1,6 +1,7 @@
 import math
 import cmath
 
+
 print('\nНу привет, давай помогу решить тебе квадратное уравнение :-)\n')
 print('Формула квадратного  уравнения: ax^2 + bx + c = 0')
 print('Мне нужны значения a, b и с')
@@ -8,7 +9,7 @@ a = complex(input('a = '))
 b = complex(input('b = '))
 c = complex(input('c = '))
 
-discr = b ** 2 - 4 * a * c
+discr = b**2 - 4*a*c
 print('Дискриминант D = %s' % discr)
 if a == 0 and b == 0 and c == 0:
     print('Корнем является любое число')
@@ -17,16 +18,16 @@ else:
         print ('решений нет')
     else:
         if a == 0 and c == 0:
-            print('x=',0)
+            print('x=', 0)
         else:
             if a == 0:
                 x = (-c / b)
-                print('x=',x)
+                print('x=', x)
             else:
                 if b == 0:
                     print('x=', math.sqrt(-c / a))
                 else:
-                    d = b*b-4*a*c
+                    d = b*b - 4*a*c
                     x1 = (-b + cmath.sqrt(d))/(2*a)
                     x2 = (-b - cmath.sqrt(d))/(2*a)
                     print('x1=',x1)
