5210132080f9d230fd2cc06a3d34c4199aa5f20c
litriq <119163655+litriq@users.noreply.github.com>
2022-12-21 14:03:02 +0300

    Update less5_2.py

--- less5_2.py
+++ less5_2.py
@@ -1,11 +1,20 @@
 def func(z = 0, *args):
+    """Сложение строк или чисел
+        
+    Принимает:
+        z - для записи суммы переменных
+        *args - переменные, которые будут складываться
+
+    Возвращает:
+        z - сумму переменных
+    """
     if type(args) == str:
         z = ''
     if type(args) == int or type(args) == float:
         z = 0
     for num in args:
         z += num
-    print(z)
+    return z
 
 func(10,1)
 func(10)
