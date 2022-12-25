55a53762df93eecca64ad4ecc9bdd96611e2de24
litriq <119163655+litriq@users.noreply.github.com>
2022-12-25 15:07:04 +0300

    Update less8_4_3.py

--- less8_4_3.py
+++ less8_4_3.py
@@ -3,13 +3,13 @@ import time
 def decor(func):
     def wrapper():
         time.sleep(1)
-        print('Столица Нигерии? Сложновато, но я обязательно вычислю и дам ответ...')
+        print('Столица Нигерии? Сложновато, но я обязательно вспомню и дам ответ...')
         time.sleep(7)
         func()
     return wrapper
 
 @decor
-def add(*args):
+def hehe(*args):
     print('Абуджа')
 
-add()
+hehe()
